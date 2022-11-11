72f457977798ebd50d9e585cedf828ee417965aa
sanyam238 <82820362+sanyam238@users.noreply.github.com>
2022-11-11 19:21:15 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){}
+
+function setup(){
+    canvas = createCanvas(700,600);
+    canvas.center();
+}
+
+function draw(){}
+
+    function take_snapshot(){
+        save("mustache.png")
+    }
